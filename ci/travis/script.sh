eb2275829f3d3a2c5d59357163eb8f5456a146af
Sebastian Dietrich <SebDieBln@users.noreply.github.com>
2017-06-25 18:26:58 +0200

    Check also indentation for XML-files, ignoring blank lines

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -4,8 +4,14 @@ set -e
 
 xmllint --version
 
+export XMLLINT_INDENT="    "
+
 for OUTPUT in $(find . -name \*.xml)
 do
     echo Checking XML syntax of $OUTPUT
     xmllint --noout $OUTPUT
+
+    echo Checking XML indentation of $OUTPUT
+    xmllint --format $OUTPUT | diff --ignore-blank-lines --to-file=- $OUTPUT
+
 done
